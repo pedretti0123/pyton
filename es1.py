9ba9e7053fe72f8c1691b2ae82381acf9225f09b
pedretti0123 <samuele.pedretti@alessandrinimainardi.edu.it>
2025-03-04 13:03:19 +0100

    Add files via upload

---
+++ es1.py
@@ -0,0 +1,15 @@
+citta=['milano','roma','napoli']
+tmax=[28,31,34]
+tmin=[18,21,24]
+i=0
+for i in range(tmin):
+    i+= tmin[i]
+    print(f"{i}")
+for i in range(tmin):
+    i+= tmin[i]
+    print(f"{tmax}")
+
+
+
+
+
